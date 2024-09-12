71b72a4e246afd719dde0109595212310cc46675
mvbasbas <86617546+mvbasbas@users.noreply.github.com>
2024-09-13 03:01:50 +0800

    upadated 2024 2

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Img from '../images/me5.svg';
+import Img from '../images/me.jpg';
 import Cp from '../images/telephone.svg';
 import Linkedin from '../images/linkedin.png';
 import Email from '../images/email.svg';
