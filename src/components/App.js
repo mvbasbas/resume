7c921a46f513ea34757131f9f0c53d8467265783
mvbasbas <86617546+mvbasbas@users.noreply.github.com>
2024-10-01 11:43:52 +0800

    update

--- src/components/App.js
+++ src/components/App.js
@@ -19,7 +19,6 @@ class App extends React.Component{
                 <Experience/>
                 <Education/>
                 <Skills/>
-                <Projects/>
                 <Achievements/>
             </div>
         );
